dee64ac33e97fad5242963cef473e0d9e733aaed
duplo77 <45257761+duplo77@users.noreply.github.com>
2018-12-13 09:12:27 +0100

    Add files via upload

--- Projekt 2 List/js/script.js
+++ Projekt 2 List/js/script.js
@@ -1,91 +1,81 @@
-/******************************************
-Treehouse Techdegree: project 2 - List Filter and Pagination
-Christian Oosterhof
-******************************************/
-
 // global variables
+const page = document.querySelector('.page')
 const eachStudent = document.querySelectorAll('.student-item');
-const buttonDiv = document.querySelector('.pagination');
-const buttonUl = buttonDiv.querySelector('ul');
 const studentsPerPage = 10;
 
-// Function for determining the rounded number of pages based on the number of students
-function numberOfPages() {
-    let pages = Math.ceil(eachStudent.length / studentsPerPage);
-    return pages;
+// To hide all students and show only a particular set of ten
+function showPage(pageNumber, studentList) {
+
+	// hide students
+	for(let i = 0; i < studentList.length; i++) {
+		studentList[i].style.display = 'none';
+	}
+
+	// loop through students in student list
+	for(let i = 0; i < studentList.length; i++) {
+		// if student belong, show them
+		if ( (i+1) >= (pageNumber * 10 - 9) && (i+1) <= (pageNumber * 10)  ) {
+			let studentItem = studentList[i];
+			studentItem.style.display = 'block';
+		}
+	}
 }
 
-// Loop create page buttons based on number of required pages
-for (let i = 1; i <= numberOfPages(); i++) {
-    let pageli = document.createElement('li');
-    let pageLink = document.createElement('a'); //Hyperlink
-    pageLink.href = '#'; // to top of the page
-    pageLink.textContent = i; //page number
-    buttonUl.appendChild(pageli);  // Append the li to buttonUl
-    pageli.appendChild(pageLink); // Append the a to li
-}
 
-const paginationLinks = document.querySelectorAll('.pagination a');
-paginationLinks[0].className = 'active';
+// function that creates all the pagination buttons, adds them to the DOM and adds their functionality.
+function appendPageLinks(studentList) {
 
-for (let i = 0; i < paginationLinks.length; i++) {
+	// Function for determining the rounded number of pages based on the number of students
+	const nrOfPages = Math.ceil(eachStudent.length / studentsPerPage);
+
+	// create a pagination section and give class 'pagination'
+	const pagination = document.createElement('div');
+	pagination.className = 'pagination';
+
+	// add opening unordered list within pagination section
+	pagination.innerHTML += '<ul>';
+
+    // add links (for every page, add li and a tags with the page number text)
+	for(let i=0; i < nrOfPages; i++) {
+		pagination.innerHTML += '<li>' + '<a href="#">' + (i+1) + '</a>' + '</li>';
+	}
+
+	// add closing unordered list tag
+	pagination.innerHTML += '</ul>';
+
+	// append the pagination section to the page
+	page.appendChild(pagination);
+
+	// store pagination links in a var = paginationLinks
+    const paginationLinks = document.querySelectorAll('.pagination a');
+
+	// link 1 active standardly
+	paginationLinks[0].className = 'active';
+
+	// event listener 'click' to all links
+	for (let i =0; i < paginationLinks.length; i++) {
 		paginationLinks[i].addEventListener('click', () => {
-		// remove active class from all the links
-		for (let i = 0; i < paginationLinks.length; i++) {
+
+		// remove active class from all links
+		for (let i=0; i < paginationLinks.length; i++) {
 			paginationLinks[i].className='';
 		}
-		// mark that link as “active”
+
+		// store the clicked number in a variable named clickedPageNumber
+		let clickedPageNumber = paginationLinks[i].textContent;
+
+		// use the showPage function to display the page for the link clicked
+		showPage(clickedPageNumber, eachStudent);
+
+		// save that link as “active”
 		paginationLinks[i].className = 'active';
 		});
 	}
-
-// Function to automatically show first ten students when page loads
-function showFirstTen() {
-    for (let i = 0; i < eachStudent.length; i++) {
-        if (i < studentsPerPage) {
-            eachStudent[i].style.display = '';
-        } else {
-            eachStudent[i].style.display = 'none';
-        }
-    }
 }
 
-/***
-   Create the `showPage` function to hide all of the items in the
-   list except for the ten you want to show.
 
-   Pro Tips:
-     - Keep in mind that with a list of 54 students, the last page
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when
-       you initially define the function, and it acts as a variable
-       or a placeholder to represent the actual function `argument`
-       that will be passed into the parens later when you call or
-       "invoke" the function
-***/
+// show first page standardly
+showPage(1, eachStudent);
 
-
-
-
-/***
-   Create the `appendPageLinks function` to generate, append, and add
-   functionality to the pagination buttons.
-***/
-
-// Event listener to divide students between the pages
-buttonDiv.addEventListener('click', (event) => {
-    let buttonNumber = parseInt(event.target.textContent);
-    let max = buttonNumber * 10;
-    let min = max - 10;
-    for (let i = 0; i < eachStudent.length; i++) {
-        if (i >= min && i < max) {
-            eachStudent[i].style.display = '';
-        }  else {
-            eachStudent[i].style.display = 'none';
-        }
-    }
-});
-
-// Function call to display first ten students on load
-showFirstTen();
+// run appendPageLinks
+appendPageLinks(eachStudent);
